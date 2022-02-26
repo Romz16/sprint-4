f793adc1758b7a581d6cadeff22a17a9a8c1c4f7
Rômulo Júnio <86726833+Romz16@users.noreply.github.com>
2022-02-25 23:34:33 -0300

    Build: Muda o server para porta 8080

--- src/server.ts
+++ src/server.ts
@@ -8,5 +8,4 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
-
-app.listen(3333,()=> console.log("Server is running on port 3333"));
+app.listen(8080,()=> console.log("Server is running on port 8080"));
